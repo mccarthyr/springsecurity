a943bc5694e76ff5ece626285728b1fcd30abdd7
mccarthyr <mccarthy.richard@gmail.com>
2017-09-29 15:48:14 +0100

    Updated the service interface and implementation to work with ACL using MutableAclService and related code

--- src/main/java/com/fireduptech/springsecurity/service/AthleteAccountService.java
+++ src/main/java/com/fireduptech/springsecurity/service/AthleteAccountService.java
@@ -2,26 +2,48 @@ package com.fireduptech.springsecurity.service;
 
 import java.util.List;
 
+import com.fireduptech.springsecurity.domain.AthleteAccountDetails;
+
 import org.springframework.security.access.annotation.Secured;
 
-import com.fireduptech.springsecurity.domain.AthleteAccountDetails;
+import org.springframework.security.access.prepost.PostFilter;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 
 public interface AthleteAccountService {
 
-	@Secured( "ROLE_ATHLETE" )
+	//@Secured( "ROLE_ATHLETE" )
+	@PreAuthorize( " hasPermission( #athleteAccountId, 'com.fireduptech.springsecurity.domain.AthleteAccountDetails', read ) or " 
+								+ " hasPermission( #athleteAccountId, 'com.fireduptech.springsecurity.domain.AthleteAccountDetails', admin ) " )
 	AthleteAccountDetails getAthleteAccount( int athleteAccountId  );
 
-	@Secured( "ROLE_ADMIN" )
+	//@Secured( "ROLE_ADMIN" )
+	@PreAuthorize( " hasPermission( 'ROLE_ADMIN' ) " )
+	@PostFilter( " hasPermission( filterObject, read ) or hasPermission( filterObject, admin ) " )
 	List<AthleteAccountDetails> getAllAthleteAccounts();
 
-	@Secured( "ROLE_ATHLETE" )
+	//@Secured( "ROLE_ATHLETE" )
+	@PreAuthorize( " hasRole( 'ROLE_ATHLETE' ) " )
 	void saveAthleteAccount(AthleteAccountDetails athleteAccountDetails );
 
-	@Secured( "ROLE_ATHLETE" )
+	//@Secured( "ROLE_ATHLETE" )
+	@PreAuthorize( " hasPermission( #athleteAccountDetails, write ) " )
 	void editAthleteAccount( AthleteAccountDetails athleteAccountDetails );
 
-	@Secured( "ROLE_ADMIN" )
+	//@Secured( "ROLE_ADMIN" )
+	@PreAuthorize( " hasPermission( #athleteAccountId, 'com.fireduptech.springsecurity.domain.AthleteAccountDetails', delete ) " )
 	void closeAthleteAccount( int athleteAccountId );
 
+
+	/*
+	  ****** FURTHER SAMPLE OPTIONS TO BUILD IN LATER ******
+		@PreAuthorize("hasPermission(#fixedDepositId, 'sample.spring.chapter16.domain.FixedDepositDetails', write)")
+		void provideAccessToAdmin(int fixedDepositId);
+
+		@PreAuthorize("hasRole('ROLE_CUSTOMER')")
+		List<FixedDepositDetails> getFixedDeposits(String user);
+	*/
+
+
 }
+
--- src/main/java/com/fireduptech/springsecurity/service/AthleteAccountServiceImpl.java
+++ src/main/java/com/fireduptech/springsecurity/service/AthleteAccountServiceImpl.java
@@ -5,6 +5,17 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import org.springframework.security.acls.domain.BasePermission;
+import org.springframework.security.acls.domain.ObjectIdentityImpl;
+import org.springframework.security.acls.domain.PrincipalSid;
+import org.springframework.security.acls.model.MutableAcl;
+import org.springframework.security.acls.model.MutableAclService;
+import org.springframework.security.acls.model.NotFoundException;
+import org.springframework.security.acls.model.ObjectIdentity;
+import org.springframework.security.acls.model.Permission;
+import org.springframework.security.acls.model.Sid;
+import org.springframework.security.core.context.SecurityContextHolder;
+
 import com.fireduptech.springsecurity.dao.AthleteAccountDao;
 import com.fireduptech.springsecurity.domain.AthleteAccountDetails;
 
@@ -14,6 +25,30 @@ public class AthleteAccountServiceImpl implements AthleteAccountService {
 
 	@Autowired
 	private AthleteAccountDao athleteAccountDao;
+
+	@Autowired
+	private MutableAclService mutableAclService;
+
+
+
+	private void addPermission( long athleteAccountId, Sid recipient, Permission permission ) {
+
+		MutableAcl acl;
+		ObjectIdentity oid = new ObjectIdentityImpl( AthleteAccountDetails.class, athleteAccountId );
+
+		try {
+			acl = (MutableAcl) mutableAclService.readAclById( oid );
+		} catch ( NotFoundException nfe ) {
+			acl = mutableAclService.createAcl( oid );
+		}
+
+		acl.insertAce( acl.getEntries().size(), permission, recipient, true );
+		mutableAclService.updateAcl( acl );
+
+		System.out.println( "Added permission " + permission + " Sid " 
+				+ recipient + " athleteAccountId " + athleteAccountId );
+	}
+
 
 	@Override
 	public AthleteAccountDetails getAthleteAccount( int athleteAccountId  ) {
@@ -27,18 +62,45 @@ public class AthleteAccountServiceImpl implements AthleteAccountService {
 
 	@Override
 	public void saveAthleteAccount(AthleteAccountDetails athleteAccountDetails ) {
+		
 		athleteAccountDao.saveAthleteAccount( athleteAccountDetails );
+
+		addPermission( athleteAccountDetails.getId(), 
+						new PrincipalSid( SecurityContextHolder.getContext().getAuthentication().getName() ), 
+						BasePermission.READ );
+		addPermission( athleteAccountDetails.getId(), 
+						new PrincipalSid( SecurityContextHolder.getContext().getAuthentication().getName() ), 
+						BasePermission.WRITE );
 	}
+
 
 	@Override
 	public void editAthleteAccount( AthleteAccountDetails athleteAccountDetails ) {
 		athleteAccountDao.editAthleteAccount( athleteAccountDetails );
 	}
 
+
 	@Override
 	public void closeAthleteAccount( int athleteAccountId ) {
+
 		athleteAccountDao.closeAthleteAccount( athleteAccountId );
+
+		ObjectIdentity oid = new ObjectIdentityImpl( AthleteAccountDetails.class, athleteAccountId );
+		mutableAclService.deleteAcl( oid, false );
 	}
+
+	/*
+	 ****** FURTHER SAMPLE OPTION TO BUILD IN LATER ******
+	 	@Override
+		public void provideAccessToAdmin(int fixedDepositId) {
+			addPermission(fixedDepositId, new PrincipalSid("admin"),
+					BasePermission.READ);
+			addPermission(fixedDepositId, new PrincipalSid("admin"),
+					BasePermission.ADMINISTRATION);
+			addPermission(fixedDepositId, new PrincipalSid("admin"),
+					BasePermission.DELETE);
+		}
+	*/
 
 }
 
